3d4eb7e21356b3122871f2c204d607740bf80ca5
ErPeter <er.peter96@gmail.com>
2020-04-20 14:31:52 +0200

    [Implementations] Implementing Point MyLine classes,adding some functions to them

---
+++ src/main/java/erpeter/szakdolgozat/Point.java
@@ -0,0 +1,29 @@
+package erpeter.szakdolgozat;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Point {
+    @Getter
+    private double xCoordinate;
+    @Getter
+    private double yCoordinate;
+    @Getter @Setter
+    private int primaryPlaceValue; //Primary indication of how good is the place of the point (0 is the best value)
+    @Getter @Setter
+    private int secondaryPlaceValue; //Secondary indication of how good is the place of the point
+
+
+    public Point(double xCoordinate, double yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.primaryPlaceValue = 0;
+        this.secondaryPlaceValue = 0;
+    }
+
+    public void setCoordinates(double xCoordinate, double yCoordinate){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+}
